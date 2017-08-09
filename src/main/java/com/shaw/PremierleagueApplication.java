2c2d8108c58319a4e0bb065f0a4e7bb6854bedf1
nicoxu <nicoxu1994@outlook.com>
2017-08-09 14:45:00 +0800

    取消在启动类中使用MapperScan,改为直接在Mapper类前加@Mapper注解

--- src/main/java/com/shaw/PremierleagueApplication.java
+++ src/main/java/com/shaw/PremierleagueApplication.java
@@ -1,11 +1,9 @@
 package com.shaw;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.shaw.schedule.mapper")
 public class PremierleagueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PremierleagueApplication.class, args);
--- src/main/java/com/shaw/schedule/mapper/TblScheduleMapper.java
+++ src/main/java/com/shaw/schedule/mapper/TblScheduleMapper.java
@@ -2,12 +2,14 @@ package com.shaw.schedule.mapper;
 
 import com.shaw.base.MyMapper;
 import com.shaw.entity.TblTeam;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 /**
  * Created by shaw on 2017/7/17.
  */
+@Mapper
 public interface TblScheduleMapper extends MyMapper<TblTeam>{
     List<TblTeam> getTeam();
 }
